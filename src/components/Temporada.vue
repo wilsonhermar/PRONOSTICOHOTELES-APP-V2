50d5c4ea2d54a281719ff16ae91562e2b107abb7
Wilson hernandez martinez <wilsonhermar@gmail.com>
2020-12-21 16:52:22 -0500

    Aun mas estilo

--- src/components/Temporada.vue
+++ src/components/Temporada.vue
@@ -87,7 +87,7 @@ export default {
     #temporada h3{
         text-align:center;
         font-family: "Homer Simpson UI";
-        color: #db0c0cbe;
+        color: #000000;
         font-size: 4.5vh;
         margin-top: 1%;
     }
